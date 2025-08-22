2b72fc6561bc256e765503f46d75ed76c92f7609
sravanipopuri2006 <popurisirisha75@gmail.com>
2025-08-22 06:18:15 +0530

    commit(22/8)

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -116,16 +116,16 @@ export const updateProfile = async(req,res) => {
     try{
         const {fullname,email,phoneNumber,bio,skills}=req.body;
         const file = req.file;
-        if (!fullname || !email || !phoneNumber || !bio || !skills) {
-            return res.status(400).json({ 
-                message: "All fields are required", 
-                success:false,
-            });
-        };
+      
 
         // cloudinary comes here
+        let skillsArray;
+        if(skills){
+            skillsArray = skills.split(",");
 
-        const skillsArray = skills.split(",");
+        }
+
+        
         const userId=req.id; //middleware authentication
         let user=await User.findById(userId);
         if(!user)
@@ -135,12 +135,13 @@ export const updateProfile = async(req,res) => {
                 success:false
             })
         }
+
         // updating data
-        user.fullname=fullname,
-        user.email=email,
-        user.phoneNumber=phoneNumber,
-        user.profile.bio=bio,
-        user.profile.skills=skillsArray
+        if(fullname)user.fullname=fullname
+        if(email)user.email=email
+        if(phoneNumber)user.phoneNumber=phoneNumber
+        if(bio)user.profile.bio=bio
+        if(skills)user.profile.skills=skillsArray
 
         // resume comes later here...
         await user.save();
